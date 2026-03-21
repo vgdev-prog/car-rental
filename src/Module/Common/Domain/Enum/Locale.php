7107cd805616e610c2f98aa35cb90bda29300928
Vladyslav Honchar <dr.tienam123@gmail.com>
2026-03-21 23:31:53 +0200

    common(domain): add Locale enum, TranslatableField value object and Doctrine type

---
+++ src/Module/Common/Domain/Enum/Locale.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace App\Module\Common\Domain\Enum;
+
+enum Locale:string
+{
+    case ENGLISH = 'en';
+    case FRENCH = 'fr';
+}
---
+++ src/Module/Common/Domain/ValueObject/TranslatableField.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Module\Common\Domain\ValueObject;
+
+use App\Module\Common\Domain\Enum\Locale;
+
+class TranslatableField implements \JsonSerializable
+{
+    public function __construct(
+        private array $translations = [],
+    )
+    {
+    }
+
+    public function get(string $locale): ?string
+    {
+        $fallback = Locale::ENGLISH->value;
+        $locale = Locale::from($locale);
+
+        return $this->translations[$locale->value]
+            ?? $this->translations[$fallback]
+            ?? null;
+    }
+
+    public function set(Locale $locale, mixed $value): self
+    {
+        return new self([...$this->translations, $locale->value => $value]);
+    }
+
+    public function all(): array
+    {
+        return $this->translations;
+    }
+
+    public static function fromArray(?array $data): self
+    {
+        return new self($data ?? []);
+    }
+
+    public function jsonSerialize(): mixed
+    {
+        return $this->translations;
+    }
+}
